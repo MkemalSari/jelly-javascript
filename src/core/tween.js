6af2e5f05f67a4908d3c49bf584b98db6dd67365
MrPeteB <MrPeteB@0fb9a3fc-dd7a-11dd-a8aa-93fc2732f0ee>
2010-02-09 17:32:38 +0000

    Major overhaul of most of the code, now with the ability to tween custom properties like rotation

--- src/core/tween.js
+++ src/core/tween.js
@@ -13,7 +13,6 @@ var Class = defineClass( 'Tween', {
 		},
 
 		__static: {
-
 			uid: 0,
 			tweens: {},
 			timerSpeed: 20,
@@ -38,7 +37,6 @@ var Class = defineClass( 'Tween', {
 			},
 
 			startTimer: function () {
-				//log('Timer started')
                 var handler = function () {
 						for ( var key in Class.tweens ) {
 							Class.tweens[ key ]();
@@ -49,12 +47,10 @@ var Class = defineClass( 'Tween', {
 
 			stopTimer: function () {
 				if ( Class.timerHandle ) {
-					//log( 'Timer stopped ')
 					clearInterval( Class.timerHandle );
 				}
 				Class.timerHandle = null;
-			}
-
+			}			
 		},
 
 		easing: J.easings.sineInOut,
@@ -111,24 +107,21 @@ var Class = defineClass( 'Tween', {
 		},
 
 		start: function ( obj ) {
-		
 			var self = this,
-				args = toArray( arguments ),
-				elem = self.element;
+				args = toArray( arguments );				
 
 			if ( args.length > 1 ) {
 				obj = {};
-				obj[args[0]] = args[1];
+				obj[ args[0] ] = args[1];
 			}
 			self.stop();
 			self.stack = [];
 
-			// Group changes are base on the first element
-			if ( isArray( elem ) || isNodeList( elem ) ) {
-				elem = elem[0];
+			// Meta properties
+			if ( 'element' in obj ) {
+				self.setElement( obj.element );
+				delete obj.element;
 			}
-
-			// Special treatment for
 			if ( 'duration' in obj ) {
 				self.setDuration( obj.duration );
 				delete obj.duration;
@@ -137,75 +130,33 @@ var Class = defineClass( 'Tween', {
 				self.setEasing( obj.easing );
 				delete obj.easing;
 			}
-
-			// Enumerate the start object to create a stack
+			
+			// Parse the start object to create the effect stack
 			enumerate( obj, function ( prop, value ) {
-				var key = camelize( prop ), extra = null;
+				var key = camelize( prop ), 
+					element = self.element[0],
+					parsers = Class.parsers,
+					parser = prop in parsers && parsers[ prop ].get ? prop : '_default',
+					customEasing = null;
 				
-				// Parsing colour values
-				if ( contains( prop, 'color' ) ) {
-					if ( isArray( value ) ) {
-						value = [
-                            parseColour( value[0], 'rgb-array' ),
-                            parseColour( value[1], 'rgb-array' ) ];
-					}
-					else {
-						var style = getStyle( elem, key );
-						if ( isNaN( style ) && !isString( style ) ) {
-							return logWarn( 'getStyle for "%s" returns NaN', key );
-						}
-						value = [
-							parseColour( style, 'rgb-array' ),
-							parseColour( value, 'rgb-array' ) ];
-					}
-					extra = 'color';
+				// Deal with object format arguments
+				if ( isObject( value ) ) {
+					customEasing = J.easings[ value.easing ];
+					value = 'from' in value ? [ value.from, value.to ] : value.to;
 				}
 				
-				// Parsing background-position values
-				else if ( prop === 'background-position' ) {
-					if ( isArray( value[0] ) ) {
-						value = [ value[0][0], value[0][1]], [value[1][0], value[1][1] ];
-					}
-					else {
-                        var startX = 0,
-							startY = 0,
-							current = getStyle( elem, key ),
-							m = /(\d+)[\w%]{1,2}\s+(\d+)[\w%]{1,2}/.exec( current );
-						if ( current && m ) {
-							startX = parseInt( m[1], 10 );
-							startY = parseInt( m[2], 10 );
-						}
-						value = [ [ startX, value[0] ], [ startY, value[1] ] ];
-					}
-					extra = 'bgp';
-				}
+				// Parse from/to values from the element 
+				var from_to = parsers[ parser ].get( self, key, value, element );
 				
-				// Parsing default 
-				else {
-					if ( !isArray( value ) ) {
-						var style = parseFloat( getStyle( elem, key ) );
-						if ( isNaN( style ) && !isString( style ) ) {
-							return logWarn( 'getStyle for "%s" returns NaN', key );
-						}
-						value = [ style, value ];
-                    }
-					else {
-						value = value;
-					}
-					if ( prop === 'opacity' ) {
-						extra = 'opac';
-					}
-				}
-				
-				// Add to effect stack
                 self.stack.push({
-                    prop: key,
-                    from: value[0],
-                    to: value[1],
-                    extra: extra
+					parser: parsers[ parser ], 
+                    prop: parsers[ parser ].prop || key,
+                    from: from_to[0],
+                    to: from_to[1],
+					easing: customEasing || self.easing
                 });
 			});
-			
+			log(self.stack)
 			self.startTime = +( new Date );
 			self.tweenId = ++( Class.uid );
             Class.subscribe( self );
@@ -231,77 +182,202 @@ var Class = defineClass( 'Tween', {
             self.increase();
 		},
 		
-		tidyUp: function () {
+		increase: function () {
 			var self = this,
 				collection = self.element,
-				stackCounter = self.stack.length - 1;
+				stackCounter = self.stack.length - 1,
+				collectionCounter,
+				item, 
+				element;
 			do {
-				var item = self.stack[ stackCounter ], 
-					extra = item.extra,
-					unit = self.unit,
-					collectionCounter = collection.length - 1;
+				item = self.stack[ stackCounter ];
+				collectionCounter = collection.length - 1;
 				do {
-					var elem = collection[ collectionCounter ], 
-						style = elem.style;
-					switch ( extra ) {
-						case 'opac':
-							setOpacity( elem, item.to );                 
-							break;
-						case 'color':
-							style[ item.prop ] = 'rgb(' + item.to.join(',') + ')';
-						case 'bgp':
-							style.backgroundPosition = item.to[0] + unit + ' ' + item.to[1] + unit;
-							break;
-						default:
-							style[ item.prop ] = item.to + unit; 
-					}
+					element = collection[ collectionCounter ]; 
+					element.style[ item.prop ] = item.parser.step( self, item, element );
 				} while ( collectionCounter-- );
 			} while ( stackCounter-- );
 		},
 		
-		increase: function () {
+		tidyUp: function () {
 			var self = this,
-				round = Math.round,
-				roundPx = !browser.firefox && self.unit === 'px',
 				collection = self.element,
-				stackCounter = self.stack.length - 1;
+				stackCounter = self.stack.length - 1,
+				collectionCounter,
+				item, 
+				element;
 			do {
-				var item = self.stack[ stackCounter ],
-					extra = item.extra,
-					unit = self.unit,
-					collectionCounter = collection.length - 1;
+				item = self.stack[ stackCounter ];
+				collectionCounter = collection.length - 1;
 				do {
-					var elem = collection[ collectionCounter ], 
-						style = elem.style;
-					switch ( extra ) {
-						case 'opac':
-							setOpacity( elem, self.compute( item.from, item.to ) );         
-							break;
-						case 'color':
-							style[ item.prop ] = 'rgb(' + 
-								round( self.compute( item.from[0], item.to[0] ) ) + ',' +
-								round( self.compute( item.from[1], item.to[1] ) ) + ',' +
-								round( self.compute( item.from[2], item.to[2] ) ) + ')';
-							break;
-						case 'bgp':
-							style.backgroundPosition = 
-								self.compute( item.from[0], item.to[0] ) + unit + ' ' + 
-								self.compute( item.from[1], item.to[1] ) + unit;
-							break;
-						default:
-							var computed = self.compute( item.from, item.to );
-							style[ item.prop ] = ( roundPx ? round( computed ) : computed ) + unit;
-					}
+					element = collection[ collectionCounter ]; 
+					element.style[ item.prop ] = item.parser.finish( self, item, element );
 				} while ( collectionCounter-- );
 			} while ( stackCounter-- );
 		},
-
-		compute: function ( from, to ) {
-			return this.easing( this.elapsedTime, from, ( to - from ), this.duration );
+		
+		compute: function ( obj, from, to ) {
+			return obj.easing( this.elapsedTime, from, ( to - from ), this.duration );
 		}
 
 	});
 
+
+// Parsers 
+var	_default = {
+		get: function ( self, key, value, element ) {
+			if ( !isArray( value ) ) {
+				var currentStyle = parseFloat( getStyle( element, key ) );
+				return [ currentStyle, value ];
+			}
+			return value;
+		},
+		step: function ( self, obj ) {	
+			return self.compute( obj, obj.from, obj.to ) + self.unit;
+		},
+		finish: function ( self, obj ) { 
+			return obj.to + self.unit;
+		}
+	},
+
+	_unitless = {
+		get: _default.get,
+		step: function ( self, obj ) {
+			return self.compute( obj, obj.from, obj.to );
+		},
+		finish: function ( self, obj ) {
+			return obj.to;
+		}
+	},
+	
+	_color = {
+		get: function ( self, key, value, element ) {
+			if ( isArray( value ) ) {
+				return [
+					parseColor( value[0], 'rgb-array' ),
+					parseColor( value[1], 'rgb-array' ) ];
+			}
+			var currentStyle = getStyle( element, key );
+			return [
+				parseColor( currentStyle, 'rgb-array' ),
+				parseColor( value, 'rgb-array' ) ];
+		},
+		step: function ( self, obj, element ) {
+			var round = Math.round;
+			return 'rgb(' +
+					round( self.compute( obj, obj.from[0], obj.to[0] ) ) + ',' +
+					round( self.compute( obj, obj.from[1], obj.to[1] ) ) + ',' +
+					round( self.compute( obj, obj.from[2], obj.to[2] ) ) + ')';
+		},
+		finish: function ( self, obj ) {
+			return 'rgb(' + obj.to.join( ',' ) + ')';
+		}
+	},
+		
+	_backgroundPosition = {
+		get: function ( self, key, value, element ) {
+			if ( isArray( value[0] ) ) {
+				return [ value[0][0], value[0][1]], [value[1][0], value[1][1] ];
+			}
+			var startX = 0,
+				startY = 0,
+				currentStyle = getStyle( elem, key ),
+				m = /(\d+)[\w%]{1,2}\s+(\d+)[\w%]{1,2}/.exec( currentStyle );
+			if ( currentStyle && m ) {
+				startX = parseInt( m[1], 10 );
+				startY = parseInt( m[2], 10 );
+			}
+			return [ [ startX, value[0] ], [ startY, value[1] ] ];
+		},
+		step: function ( self, obj ) {
+			return 	self.compute( obj, obj.from[0], obj.to[0] ) + self.unit + ' ' + 
+					self.compute( obj, obj.from[1], obj.to[1] ) + self.unit;
+		},
+		finish: function ( self, obj ) {
+			return obj.to[0] + self.unit + ' ' + obj.to[1] + self.unit;
+		}
+	},
+
+	_opacity = function () {
+		if ( 'opacity' in docRoot.style ) {
+			return _unitless;
+		}
+		return {
+			get: function ( self, key, value, element ) {
+				if ( !isArray( value ) ) {
+					var elStyle = element.style;
+					if ( elStyle.opacity === undefined ) {
+						elStyle.opacity = 1;
+						elStyle.zoom = 1;
+					}
+					return [ elStyle.opacity, value ];
+				}
+				return value;
+			},
+			step: function ( self, obj, element ) {
+				var result = self.compute( obj, obj.from, obj.to )
+				element.style.filter = result === 1 ? '' : 'alpha(opacity=' + ( result * 100 ) + ')';
+				return result;
+			},
+			finish: function ( self, obj, element ) {
+				element.style.filter = obj.to === 1 ? '' : 'alpha(opacity=' + ( obj.to * 100 ) + ')';
+				return obj.to;
+			}
+		}
+	}(),
+	
+	transformProperty = function () {
+		var cases = [ 'transform', 'WebkitTransform', 'MozTransform' ];
+		for ( var i = 0; i < cases.length; i++ ) {
+			if ( cases[i] in docRoot.style ) {
+				return cases[i];
+			}
+		}
+		return cases[0];
+	}(),
+	
+	_rotation = function () {
+		var rotationRe = /((?:^|\s)rotate\()([0-9]+\.?[0-9]*)(deg\))/;
+		return { 
+			prop: transformProperty,
+			get: function ( self, key, value, element ) {
+				if ( !isArray( value ) ) {
+					var elStyle = element.style,
+						currentStyle = elStyle[ transformProperty ] || 0, 
+						m = rotationRe.exec( currentStyle );
+					if ( currentStyle ) {
+						currentStyle = m ? m[2] : 0;
+					}
+					if ( !m ) { 
+						self.element.each( function ( el ) {
+							el.style[ transformProperty ] += ' rotate(' + currentStyle + 'deg)';
+						});
+					}
+					return [ currentStyle, value ];
+				}
+				return value;
+			},
+			step: function ( self, obj, element ) {	
+				var result = self.compute( obj, obj.from, obj.to );
+				return element.style[ transformProperty ].replace( rotationRe, '$1' + result + '$3' ); 
+			},
+			finish: function ( self, obj, element ) { 
+				return element.style[ transformProperty ].replace( rotationRe, '$1' + obj.to + '$3' ); 
+			}
+		}
+	}();
+
+	
+Class.parsers = {
+	_default: _default,
+	_unitless: _unitless,
+	'opacity': _opacity,
+	'color': _color,
+	'background-color': _color,
+	'background-position': _backgroundPosition,
+	'rotation': _rotation
+};
+	
 })();
 
 var Tween = J.Tween;
